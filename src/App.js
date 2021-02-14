4e5677f2a463b244e1e765429bf2e48a9136e774
rafik mami <rafik.mami94@gmail.com>
2021-02-14 16:08:22 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,18 @@
-import "./App.css";
-import React, { useState } from "react";
-import { Button } from "react-bootstrap";
-import Movielist from "./component/movieList";
-import MovieCard from "./component/movieCard";
-import Navbar from "./component/navbar";
-const page_list = "Movielist";
-const page_card = "MovieCard";
-function App() {
-  const [card, setCard] = useState([]);
-  const [page, setPage] = useState(page_list);
+  import "./App.css";
+import React from "react";
+//import ReactStars from "react-rating-stars-component";
+import List from "./component/List";
 
-  const navigateTo = (nextPage) => {
-    setPage(nextPage);
-  };
+
+function App() {
+
 
   return (
-    <div className="appMovie">
-      <header className="header">
-        <Navbar />
-      </header>
 
-      <div>
-        <Button
-          variant="primary"
-          className="btn"
-          onClick={() => navigateTo(page_card)}
-        >
-          Movie Card
-        </Button>{" "}
-        <Button
-          variant="primary"
-          className="btn"
-          onClick={() => navigateTo(page_list)}
-        >
-          Movie List
-        </Button>{" "}
+      <div >
+        <List />
       </div>
-      <div>
-        {page === page_list && <Movielist card={card} setCard={setCard} />}
-        {page === page_card && <MovieCard card={card} setCard={setCard} />}
-      </div>
-    </div>
+    
   );
 }
 
---
+++ src/component/newCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function newCard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default newCard
